63515b51678c6c8baa8244535e1b34688daaac5a
pranishchauhan2007-source <pranishchauhan2007@gmail.com>
2026-06-06 22:24:40 +0530

    Check positive and negative number

---
+++ CheckPositiveAndNegativeNumber.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("enter the number");
+scanf("%d",&n);
+if(n==0)
+{
+printf("number is zero");
+}
+if(n>0)
+{
+printf(" number is positive");
+}
+else
+{
+printf("number is negative");
+}
+}
