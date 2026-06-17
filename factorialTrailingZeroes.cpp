0c8d5acb205386ace1a4229d5a9d30c84fc64a75
pranishchauhan2007-source <pranishchauhan2007@gmail.com>
2026-06-17 18:24:45 +0530

    Factorial trailing zeroes

---
+++ factorialTrailingZeroes.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+    int main() {
+        int n, count = 0;
+        cin>>n;
+
+        while (n > 0) {
+            n /= 5;
+            count += n;
+        }
+
+        cout<< count;
+    }
+    
