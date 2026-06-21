c5978142dc583ffc658ca979a2f51925399182b4
pranishchauhan2007-source <pranishchauhan2007@gmail.com>
2026-06-21 23:21:46 +0530

    Count operation to obtain zero

---
+++ count operation to obtain zero.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n1, n2, count = 0;
+    cin >> n1 >> n2;
+
+    while (n1 != 0 && n2 != 0)
+    {
+        if (n1 > n2)
+            n1 = n1 - n2;
+        else
+            n2 = n2 - n1;
+
+        count++;
+    }
+
+    cout << count;
+    return 0;
+}
