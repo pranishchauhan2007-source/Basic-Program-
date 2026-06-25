2a40778ce030cab00770c63bd7925bbf67c9bbbb
pranishchauhan2007-source <pranishchauhan2007@gmail.com>
2026-06-25 11:19:26 +0530

    Armstrong number range counter

---
+++ Armstrong Number Range Counter .cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    int n,rem,count=0;
+    int end,digit,start;
+    cout<<"enter the start:";
+    cin>>start;
+    cout<<"enter the end:";
+    cin>>end;
+    cout<<"enter the digit:";
+    cin>>digit;
+    for(n=start;n<=end;n++)
+    {
+    int sum =0;
+    int temp=n;
+    while(temp>0)
+    {
+    rem=temp%10;
+    temp/=10;
+    sum+=(int)pow(rem,digit);
+    }
+    if(sum==n)
+    count++;
+  }
+  cout<<count;
+  }
