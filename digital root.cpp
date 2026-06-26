92fef68842516c685a5bc2e258ced8d7ffd12e16
pranishchauhan2007-source <pranishchauhan2007@gmail.com>
2026-06-26 07:54:46 +0530

    Digital root

---
+++ digital root.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n , rem, sum = 0;
+    cin>>n;
+    while (n > 9)
+    {
+        int sum = 0;
+
+        while (n > 0)
+        {
+            sum = sum + (n % 10);
+            n = n / 10;
+        }
+
+        n = sum;
+    }
+     cout<<n;
+}
