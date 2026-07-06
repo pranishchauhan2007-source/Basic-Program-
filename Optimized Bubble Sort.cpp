fcd73b5124cca8c5094ee9d974c81cd212643598
pranishchauhan2007-source <pranishchauhan2007@gmail.com>
2026-07-06 21:12:07 +0530

    Optimised bubble sort

---
+++ Optimized Bubble Sort.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[100];
+    int n;
+
+    cout << "Enter the size of array: ";
+    cin >> n;
+
+    cout << "Enter the elements of array:\n";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = n - 2; i >= 0; i--)
+    {
+        bool swapped = false;
+
+        for (int j = 0; j <= i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+
+        if (swapped == false)
+        {
+            break;
+        }
+    }
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
