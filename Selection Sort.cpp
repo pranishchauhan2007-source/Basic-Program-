40eae0980f4d05f582c0d9aff69dc1a671d478b9
pranishchauhan2007-source <pranishchauhan2007@gmail.com>
2026-07-06 21:11:02 +0530

    Selection sort

---
+++ Selection Sort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[] = {5, 2, 8, 1, 3};
+    int n = 5;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        int minIndex = i;
+
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[minIndex])
+            {
+                minIndex = j;
+            }
+        }
+
+        swap(arr[i], arr[minIndex]);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
